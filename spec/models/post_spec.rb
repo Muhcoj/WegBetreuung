41d9994afe18d0ac024735f4d6b13c7f50fb492e
Muhcoj <jochum.de.bueger@gmail.com>
2017-11-03 19:30:55 +0100

    Created Post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "Creation" do
+    before do
+      @post = Post.create(title: "My Title", content: "My content asdfasdf", date: Date.today)
+    end
+
+    it 'can be created' do
+     expect(@post).to be_valid
+    end
+
+    it 'cannot be created without title, content and date.' do
+      @post.title = nil
+      @post.content = nil
+      @post.date = nil
+      expect(@post).to_not be_valid
+    end
+  end
+end
