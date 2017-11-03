4c195f0d0c43c85bb57f56935903faf9457c0da9
Muhcoj <jochum.de.bueger@gmail.com>
2017-11-03 20:51:37 +0100

    Created Post INdex view and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |post|
+  Post.create!(title: "Some title", content: "Some Content asdfasdf", date: Date.today)
+end
+
+puts "10 Posts have been created"
