92285577b86fa01c46cb6709dd6d0d3cf583ed0c
Muhcoj <jochum.de.bueger@gmail.com>
2017-11-03 21:24:30 +0100

    Implemented new create and show funtionality and refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,35 @@
 class PostsController < ApplicationController
-  def index
-    
+  before_action :set_post, only: [:show]
+
+  def index  
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :content, :date))
+
+    if @post.save
+      redirect_to @post, notice: "Nachricht gespeichert"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :date)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
