330a43f076d9aa4e194409351af3d80568b695c6
Muhcoj <jochum.de.bueger@gmail.com>
2017-11-11 17:54:29 +0100

    Implemented ability to create new kontoumsaetze

--- app/controllers/kontoumsaetzes_controller.rb
+++ app/controllers/kontoumsaetzes_controller.rb
@@ -1,6 +1,22 @@
 class KontoumsaetzesController < ApplicationController
 
   def index
-    
   end
+
+  def new
+    @kontoumsaetze = Kontoumsaetze.new
+  end
+
+  def create
+    @kontoumsaetze = Kontoumsaetze.new(params.require(:kontoumsaetze).permit(:weg, :wertstellung, :umsatzart, :buchungsdetails, :auftraggeber, :empfaenger, :betrag, :saldo))
+
+    @kontoumsaetze.save
+
+    redirect_to @kontoumsaetze
+  end
+
+  def show
+    @kontoumsaetze = Kontoumsaetze.find(params[:id])
+  end
+
 end
--- spec/features/kontoumsaetze_spec.rb
+++ spec/features/kontoumsaetze_spec.rb
@@ -12,4 +12,27 @@ describe 'navigate' do
       expect(page).to have_content(/Kontoumsätze/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_kontoumsaetze_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from a new form page' do
+      visit new_kontoumsaetze_path
+
+      fill_in 'kontoumsaetze[weg]', with: 2
+      fill_in 'kontoumsaetze[wertstellung]', with: "08.08.2017"
+      fill_in 'kontoumsaetze[umsatzart]', with: "Gutschrift"
+      fill_in 'kontoumsaetze[buchungsdetails]', with: "Referenz 9562231945DI"
+      fill_in 'kontoumsaetze[auftraggeber]', with: "ANNEGRET"
+      fill_in 'kontoumsaetze[empfaenger]', with: "Klaus-Dieter"
+      fill_in 'kontoumsaetze[betrag]', with: 300.20
+      fill_in 'kontoumsaetze[saldo]', with: 32000.80
+      click_on "Speichern"
+
+      expect(page).to have_content("Referenz 9562231945DI")
+    end
+  end
 end
